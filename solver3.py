42c3fc17d945c01ae237427a2aac9764122a753f
Yari Anrango <yarianrango@air-van-yari.kpn>
2022-11-14 22:50:20 +0100

    Added 3 new Solvers. solver3.py is the latest.

---
+++ solver3.py
@@ -0,0 +1,74 @@
+import random
+import numpy as np
+from Reading import read
+from datetime import datetime
+
+#clauses = read('sudoku1.cnf')
+clauses = [[111,121,-131],[121,131],[-121],[-111,131]]
+all_elements = [abs(x) for l in clauses for x in l]
+a = np.array(all_elements)
+unique = np.unique(a)
+unique_elements = list(unique)
+
+def testclause(clause, trues, falses):
+    notin = 0
+    v = 0
+    for i in range(len(clause)):
+        if abs(clause[i]) not in trues and abs(clause[i]) not in falses:
+            notin += 1
+            continue
+        if abs(clause[i]) in trues and clause[i] > 0:
+            v += 1
+        elif abs(clause[i]) in falses and clause[i] < 0:
+            v += 1
+    if notin > 0:
+        return -1
+    elif v > 0:
+        return True
+    elif v == 0:
+        return False
+
+
+def test_all_clauses(clauses, trues, falses):
+    notyet = 0
+    for clause in clauses:
+        if testclause(clause, trues, falses) == -1:
+            notyet += 1
+        elif testclause(clause, trues, falses) == False:
+            return False
+    if notyet > 0:
+        return 'Not yet'
+    print('notyet',notyet, 'len clauses',len(clauses))
+    return True
+
+trues = []
+falses = []
+def dpll(clauses, unique_elements, pas):
+    global trues, falses
+    #print('trues', trues, 'falses', falses)
+    if test_all_clauses(clauses, trues, falses) == False:
+        #print('test 1')
+        if pas in trues:
+            trues.remove(pas)
+            falses += [pas]
+        return False
+    if test_all_clauses(clauses, trues, falses) == True:
+        print(trues, falses)
+        print('test 2')
+        return True
+    pa = random.choice(unique_elements)
+    while pa in falses or pa in trues:
+        #print('redrawing')
+        pa = random.choice(unique_elements)
+    falses += [pa]
+    if dpll(clauses, unique_elements, pa):
+        #print('test3')
+        return True
+    else:
+        index = falses.index(pa)
+        falses = falses[:index]
+        trues += [pa]
+        return dpll(clauses, unique_elements, pa)
+
+
+print(dpll(clauses, unique_elements, 0))
